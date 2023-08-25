28910100ec667d1f9935a4e137ca9c2921d8db18
PujitYG <pujityg@gmail.com>
2023-08-26 00:07:47 +0530

    Added docker

---
+++ auth-service/src/test/java/com/microservice/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.microservice.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
+
+
